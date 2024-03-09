a9e42d45f9871421a321f4a4b06186b7b5880507
Hayden Montgomery <hayden.montgomery815@gmail.com>
2024-03-09 09:54:04 -0500

    Initial files as well as table creation on startup

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -18,6 +18,11 @@
 //    ...
 //  ]
 
+//The new api for jeopardy only has 14 categories with the ids of 2, 3, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18  
+//Will use an array to pick which ones to randomly use
+const id = [2, 3, 4, 6, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18];
+
+
 let categories = [];
 
 
@@ -27,6 +32,12 @@ let categories = [];
  */
 
 function getCategoryIds() {
+    for (let i = id.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [id[i], id[j]] = [id[j], id[i]];
+    }
+
+    return id.slice(0,6);
 }
 
 /** Return object with data about a category:
@@ -42,6 +53,12 @@ function getCategoryIds() {
  */
 
 function getCategory(catId) {
+    const catOjbect = {
+        title: catId.data.title,
+        clues: catId.data.clues
+    }
+
+    return catOjbect;
 }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
@@ -53,6 +70,56 @@ function getCategory(catId) {
  */
 
 async function fillTable() {
+    const $body = $('body');
+    const table = document.createElement('table')
+    table.id = "jeopardy";
+    const tHead = document.createElement('tHead');
+    const trHead = document.createElement('tr');
+    const tBody = document.createElement('tbody')
+    table.classList = "table";
+    
+    for (let i = 0; i < 6; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < 6; j++) {
+            const td = document.createElement('td');
+            if (i != 0) {
+                td.addEventListener('click', handleClick);
+                td.innerText = "?"
+                td.id = `${i+1}${j+1}`
+                console.log(td.id);
+            } else {
+                td.innerText = categories[j].title;
+            }
+            tr.appendChild(td);
+        }
+        if (i == 0) {
+            tHead.appendChild(tr)
+        } else {
+            tBody.appendChild(tr);
+        }
+    }
+
+    table.appendChild(tHead);
+    table.appendChild(tBody);
+/*     for(let cat in categories) {
+        const td = document.createElement('td');
+        console.log(categories[cat].title);
+        td.innerText = categories[cat].title;
+        trHead.appendChild(td);
+    }
+    tHead.appendChild(trHead);
+    table.appendChild(tHead);
+
+    for (let i = 0; i < 6; i++){
+        const trBody = document.createElement('tr');
+        for(let cat in categories) {
+            const td = document.createElement('td');
+            console.log(categories[cat]);
+            td.innerText = categories[cat].clues;
+            trBody.appendChild(td);
+        }
+    } */
+    $body.append(table);
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -64,6 +131,15 @@ async function fillTable() {
  * */
 
 function handleClick(evt) {
+    console.log(evt.target.id);
+    let str = evt.target.id.split('')
+    let row = str[0];
+    let col = str[1];
+    if (evt.target.innerText == "?"){
+
+
+        //evt.target.innerText = question;
+    }
 }
 
 /** Wipe the current Jeopardy board, show the loading spinner,
@@ -87,6 +163,22 @@ function hideLoadingView() {
  * */
 
 async function setupAndStart() {
+    const categoryID = getCategoryIds();
+    //const res = await axios.get("https://rithm-jeopardy.herokuapp.com/api/category?id=3");
+    //const categories = []
+    try {
+        for (let cat in categoryID){
+            const res = await axios.get(`https://rithm-jeopardy.herokuapp.com/api/category?id=${categoryID[cat]}`);
+            categories[cat] = getCategory(res);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }    
+    fillTable();
+
+    console.log(categories[2]);
+        
 }
 
 /** On click of start / restart button, set up game. */
